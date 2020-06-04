c118b97eb4f8c664cc232289724cbf4f335a2650
firatmikyaz <66138781+firatmikyaz@users.noreply.github.com>
2020-06-05 01:26:36 +0300

    Add project files.

---
+++ While_Dongusu_Çiftsayilarin_teksayilardan_çıkarilmasi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Dongusu_Çiftsayilarin_teksayilardan_çıkarilmasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            int sayi = 0;
+
+            while (true)
+            {
+                Console.Write("Bir sayı giriniz?");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi == 0)
+                {
+                    break;
+                }
+
+                if (sayi % 2 == 0)
+                {
+                    toplam = toplam + sayi;
+                }
+                else
+                {
+                    toplam = toplam - sayi;
+                }
+            }
+            Console.Write("Sonuç:{0}", toplam);
+            Console.ReadKey();
+        }
+    }
+}
